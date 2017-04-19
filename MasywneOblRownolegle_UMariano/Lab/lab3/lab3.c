29099fd646b4a78810d4fed9bdbd8bc2f15b5f7a
piotr210694 <piotr210694@wp.pl>
2017-04-19 08:52:09 +0200

    #PA - rybakowa, marian, melinowska

--- MasywneOblRownolegle_UMariano/Lab/lab3/lab3.c
+++ MasywneOblRownolegle_UMariano/Lab/lab3/lab3.c
@@ -1,28 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-#include <mpi.h>
-
-#define NLOOPS = 100000000
-
-int main(int argc, char *argv[])
+class ForLoopTest 
 {
-    int id, p;
-    int i, n, nsum, np;
-    double x, y, z, wyn;
-    double tStart, tStop;
-
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &id)
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
-
-    srand(time(NULL));
-    
-    MPI_Barrier(MPI_COMM_WORLD);
-    tStart = MPI_Wtime();
-    n = 0;
-    
-
-    return 0;
+	static void Main() 
+	{
+		for (int i = 1; i <= 5; i++)
+		{
+			Console.WriteLine(i);
+		}
+	}
 }
+/*
+	Output:
+	1
+	2
+	3
+	4
+	5
+*/
